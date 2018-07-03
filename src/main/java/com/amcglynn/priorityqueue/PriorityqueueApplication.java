f0fb4f210d6c91044f73a0059893861f9d1c661e
Andrew <andrewmcglynn86@hotmail.com>
2018-07-03 21:43:18 +0100

    Initial project from Spring Initializr

---
+++ src/main/java/com/amcglynn/priorityqueue/PriorityqueueApplication.java
@@ -0,0 +1,12 @@
+package com.amcglynn.priorityqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorityqueueApplication.class, args);
+	}
+}
